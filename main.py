04bbf5cc249a3073c8d01f844899dcd0f99424e0
Arsalan Akhter <aakhter@wpi.edu>
2022-03-18 07:16:53 -0400

    Fourth commit - Working but unstable

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
+from types import SimpleNamespace
 
 QUIET = 0
-MAX_ITER = 1000
+MAX_ITER = 10
 ABSTOL = 1e-4
 RELTOL = 1e-2
 
 
 def objective(c, x):
-    return np.transpose(c) * x
+    return np.transpose(c) @ x
 
 
 def linprog(c, A, b, rho, alpha):
@@ -22,23 +23,24 @@ def linprog(c, A, b, rho, alpha):
     u = np.zeros((n, 1))
 
     history = {}
-    history['objval'] = []
-    history['r_norm'] = []
-    history['s_norm'] = []
-    history['eps_pri'] = []
-    history['eps_dual'] = []
+    history = SimpleNamespace(**history)
+    history.objval = [0 for _ in range(MAX_ITER)]
+    history.r_norm = [0 for _ in range(MAX_ITER)]
+    history.s_norm = [0 for _ in range(MAX_ITER)]
+    history.eps_pri = [0 for _ in range(MAX_ITER)]
+    history.eps_dual = [0 for _ in range(MAX_ITER)]
 
     if not QUIET:
         print(f'iter\tnorm\teps pri\ts norm\teps dual\tobjective')
 
     for k in range(MAX_ITER):
         # x-update
-        tmp1 = np.concatenate((rho * np.identity(n), np.transpose(A)), axis=0)
-        tmp2 = np.concatenate((A, np.zeros(m)), axis=0)
-        temp_neum = np.concatenate((tmp1, tmp2), axis=1)
-        tmp3 = np.block([rho*(z-u)-c], [b])
-        tmp = temp_neum / tmp3
-        x = tmp[1:n]
+        tmp1 = np.hstack((rho * np.identity(n), np.transpose(A)))
+        tmp2 = np.block([A, np.zeros((m, m))])
+        temp_neum = np.block([[tmp1], [tmp2]])
+        tmp3 = np.block([[rho*(z-u)-c], [b]])
+        tmp = np.linalg.solve(temp_neum, tmp3)
+        x = tmp[0:n]
 
         # z-update with relaxation
         z_old = z
@@ -48,15 +50,15 @@ def linprog(c, A, b, rho, alpha):
 
         # Diagnostics, reporting, termination
         history.objval[k] = objective(c, x)
-        history.r_norm[k] = np.norm(x - z)
-        history.s_norm[k] = np.norm(-rho * (z - z_old))
+        history.r_norm[k] = np.linalg.norm(x - z)
+        history.s_norm[k] = np.linalg.norm(-rho * (z - z_old))
 
-        history.eps_pri[k] = np.sqrt(n) * ABSTOL + RELTOL * max(np.norm(x), np.norm(-z))
-        history.eps_dual[k] = np.sqrt(n) * ABSTOL + RELTOL * np.norm(rho * u)
+        history.eps_pri[k] = np.sqrt(n) * ABSTOL + RELTOL * max(np.linalg.norm(x), np.linalg.norm(-z))
+        history.eps_dual[k] = np.sqrt(n) * ABSTOL + RELTOL * np.linalg.norm(rho * u)
 
-        if ~QUIET:
-            print(f'{k}\t{history.r_norm[k]}\t{history.eps_pri[k]}\t'
-                  f'{history.s_norm[k]}\t{history.eps_dual[k]}\t'
+        if not QUIET:
+            print(f'{k}\t\t{history.r_norm[k]:.3f}\t{history.eps_pri[k]:.3f}\t'
+                  f'{history.s_norm[k]:.3f}\t{history.eps_dual[k]:.3f}\t'
                   f'{history.objval[k]}')
 
         if history.r_norm[k] < history.eps_pri[k] and \
@@ -73,10 +75,10 @@ if __name__ == '__main__':
     m = 400  # number of equality constraints
 
     # create non-negative price vector with mean 1
-    c = rng.normal(n, 1) + 0.5
-    x0 = abs(rng.random(n))  # create random solution vector
+    c = rng.normal(loc=1, size=(n, 1)) + 0.5
+    x0 = abs(rng.random(size=(n, 1)))  # create random solution vector
 
     A = abs(rng.random((m, n)))  # create random, non-negative matrix A
-    b = A * x0
+    b = np.matmul(A, x0)
 
     x, history = linprog(c, A, b, 1.0, 1.0)
